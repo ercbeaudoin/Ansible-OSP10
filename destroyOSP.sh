43fbb1570812dc42b2ef32a39ca5db43cf0b4140
Eric Beaudoin <ebeaudoi@redhat.com>
2017-06-03 09:31:07 -0400

    Playbook deployed OSP10

--- destroyOSP.sh
+++ destroyOSP.sh
@@ -2,17 +2,14 @@
 echo "remove stack user"
 userdel -r stack
 
-#virsh destroy undercloud-osp8
-
-#virsh undefine undercloud-osp8
 echo "Destroy all VMs"
-for i in $(virsh list --all |grep osp8- |awk ' { print $2 } ');do virsh destroy $i;virsh undefine $i;done
+for i in $(virsh list --all |grep osp10- |awk ' { print $2 } ');do virsh destroy $i;virsh undefine $i;done
 
 echo "Remove cloud images"
-rm -f /var/lib/libvirt/images/undercloud-osp8.qcow2 /var/lib/libvirt/images/osp8-*
+rm -f /var/lib/libvirt/images/undercloud-osp10.qcow2 /var/lib/libvirt/images/osp10-*
 
 echo "Remove tmp file"
-rm -f /tmp/osp8-*
+rm -f /tmp/osp10-*
 
 echo "Remove the tmp key"
-rm -fr /tmp/osp8-undercloud/
+rm -fr /tmp/osp10-undercloud/
